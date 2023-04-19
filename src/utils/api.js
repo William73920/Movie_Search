50a14b5a8420b968fd73e79b4371824903591968
Will <rrozario958@gmail.com>
2023-04-20 01:55:32 +0530

    api configuration

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TOKEN = import.meta.env.VITE_APP_TMD_TOKEN;
+
+const headers = {
+  Authorization: "bearer " + TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
