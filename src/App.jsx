a4f5e8a85c4406db427d815cfbfaf1b86d56b270
Will <rrozario958@gmail.com>
2023-04-21 01:50:13 +0530

    pages and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageError from "./pages/404/pageError";
 
 function App() {
   const dispatch = useDispatch();
@@ -15,7 +23,19 @@ function App() {
     dispatch(getApiConfiguration(data));
   };
 
-  return <div className="App">{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="*" element={<PageError />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/404/PageError.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.scss";
+
+const PageError = () => {
+  return <div>pageError</div>;
+};
+
+export default PageError;
